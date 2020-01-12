ddf515c9ea6bbb4a143121b82b2aa92e451cd4c7
De Isaac <deisaack@gmail.com>
2020-01-13 00:33:26 +0300

    Electronics

---
+++ src/data/theme.js
@@ -0,0 +1,14 @@
+export default {
+    name: 'Hashmat Kenya',
+    fullName: 'Hashmart Online Shop',
+    url: 'https://hashmart.co.ke',
+    author: {
+        name: 'De Isaac',
+        profile_url: 'https://github.com/kipkemei',
+    },
+    contacts: {
+        address: '4190 - 00100 Nairobi',
+        email: 'isaac@soofapay.com',
+        phone: '+254721 732 519',
+    },
+};
